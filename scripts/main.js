bec89712c9b002180bc4afbe11521f24fcab4171
David Flick <davidflick@Davids-MacBook-Pro.local>
2017-01-14 16:31:56 -0500

    added JS variables

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-	
+	var $one = $('#one');
+	var $two = $('#two');
+	var $three = $('#three');
+	var $four = $('#four');
+	var $five = $('#five');
+	var $six = $('#six');
+	var $seven = $('#seven');
+	var $eight = $('#eight');
+	var $nine = $('#nine');
+	var $zero = $('#zero');
+	var $decimal = $('#decimal');
+	var $plus = $('#plus');
+	var $minus = $('#minus');
+	var $times = $('#times');
+	var $divide = $('#divide');
+	var $equals = $('#equals');
+	var $allClear = $('#allClear');
+	var $clearEntry = $('#clearEntry');
+	var $answer = $('#answerText');
+
+
+$one.click(function(){
+	$answer.append(1);
+});
+
+
+
 });
